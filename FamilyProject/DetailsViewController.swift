207adb30244337d4e96b892b5c612278a92fa0a2
Wesley Keetch <keetchcode@gmail.com>
2024-10-08 10:31:42 -0600

    pushing last changes

--- FamilyProject/DetailsViewController.swift
+++ FamilyProject/DetailsViewController.swift
@@ -8,28 +8,28 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-  
+
   var selectedFamilyMember: FamilyMember?
-  
+
   @IBOutlet weak var ImageBox: UIImageView!
-  
+
   @IBOutlet weak var nameLabel: UILabel!
-  
+
   @IBOutlet weak var ageLabel: UILabel!
-  
+
   @IBOutlet weak var relationshipLabel: UILabel!
-  
+
   @IBOutlet weak var detailsLabel: UILabel!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+
     if let member = selectedFamilyMember {
       nameLabel.text = member.name
       ageLabel.text = String(member.age)
       relationshipLabel.text = member.relationship
       detailsLabel.text = member.details
-      
+
     }
   }
 }
